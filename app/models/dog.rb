82c0717c046df9f30f38e434ed2af19674109055
Laura Messa Travin <laura.travin@gmail.com>
2019-01-23 22:46:39 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,11 @@
 class Dog
-  attr_accessor :name, :bree: age
+  attr_accessor :name, :bree,  :age
+  @all = []
   def initialization(name,breed,age)
     @name: name
     @breed: breed
     @age: age
+    @all << self
   end
 
 end
